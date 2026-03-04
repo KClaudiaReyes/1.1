46cbec97ac93db69bc641206602a6018a0e067fa
clauR <creyesccf@gmail.com>
2026-03-04 16:25:09 +0100

    inicio

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,8 @@ namespace _1._1
     {
         static void Main(string[] args)
         {
+            Dictionary <int , string > estudiantes = new Dictionary<int, string> ();
+
         }
     }
 }
